a0820c11500565c1268a744864817e060f50f902
Mario Arro <marioarro186@gmail.com>
2021-12-02 12:56:03 +0200

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -45,12 +53,11 @@ export const setExpenses = (expenses) => ({
   expenses
 });
 
-// export const startSetExpenses;
-
 export const startSetExpenses = () => {
   return (dispatch) => {
     return database.ref('expenses').once('value').then((snapshot) => {
       const expenses = [];
+
       snapshot.forEach((childSnapshot) => {
         expenses.push({
           id: childSnapshot.key,
